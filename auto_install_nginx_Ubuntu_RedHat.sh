332fe8a069f12b3c74e476634d43119330b0a1b1
Anatoliy <anatoliykv@gmail.com>
2020-03-09 18:46:26 +0200

    Update auto_install_nginx_Ubuntu_RedHat.sh

--- auto_install_nginx_Ubuntu_RedHat.sh
+++ auto_install_nginx_Ubuntu_RedHat.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-
+showOSversion() {
+    echo "My OS version is: `cat /proc/version`"
+}
+showOSversion
+grepUbuntufunc(){
+    grep "ubuntu" /proc/version
+}
 #Ubuntu
-if grep "ubuntu" /proc/version
+if grepUbuntufunc
 then
-sudo apt install -y curl gnupg2 ca-certificates lsb-release
-echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+    sudo apt install -y curl gnupg2 ca-certificates lsb-release
+    echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
     | sudo tee /etc/apt/sources.list.d/nginx.list
-curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-apt update
-apt install nginx
-#Red Hat
+    curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+    apt update
+    apt install nginx
+    #Red Hat
 elif grep "Red Hat" /proc/version
 then
-echo "[nginx]
+    echo "[nginx]
 name=nginx repo
 baseurl=http://nginx.org/packages/mainline/rhel/7/\$basearch/
 gpgcheck=0
-enabled=1" > /etc/yum.repos.d/nginx.repo
-yum update -y
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
+    enabled=1" > /etc/yum.repos.d/nginx.repo
+    yum update -y
+    yum install nginx -y
+    systemctl enable nginx
+    systemctl start nginx
 else
-echo -e "\e[41;1m Nothink to do \e[0m"
+    echo -e "\e[41;1m Nothink to do \e[0m"
 fi
