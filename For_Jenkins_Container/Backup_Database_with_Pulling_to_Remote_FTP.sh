52b5c4a3cc911c78536ff15c0d0a255c036e1b24
Anatoliy <anatoliykv@gmail.com>
2020-04-04 17:57:41 +0300

    Update Backup_Database_with_Pulling_to_Remote_FTP.sh

--- For_Jenkins_Container/Backup_Database_with_Pulling_to_Remote_FTP.sh
+++ For_Jenkins_Container/Backup_Database_with_Pulling_to_Remote_FTP.sh
@@ -4,9 +4,18 @@ mkdir $backup_folder
 cd ~/$backup_folder
 mysqldump -u $mysql_login -p$mysql_password $database_name | gzip > `date +./%d.%m.%Y.%H-%M-%S.sql.gz`
 scp -i $key_path ~/$backup_folder/*.sql.gz $ftp_path
+#Testing pushing to FTP
+if [ $? -ne 0 ]; then
+    echo "Error pushing to FTP"
+    exit 1
+else
+    echo "Archive was pushed to FTP"
+fi
 rm ~/$backup_folder/*.sql.gz
+#Testing whether archive was deleted successfully
 if [ -f *.sql.gz ]; then
     echo "Error"
+    exit 1
 else
     echo "Success!"
 fi
