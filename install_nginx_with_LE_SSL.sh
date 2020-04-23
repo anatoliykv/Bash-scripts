c08eaa8604f84cb340751bb847cd0e59ee7f7757
anatoliykv <anatoliykv@gmail.com>
2020-04-23 09:48:21 +0300

    cerbot added

--- install_nginx_with_LE_SSL.sh
+++ install_nginx_with_LE_SSL.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 echo "Enter your domain name"
-read domain_name
+read DOMAIN_NAME
+echo "Enter your email"
+read EMAIL
 sudo apt update
 sudo apt install nginx -y
+
+sed 's/danger/safety/' testfile.txt
+
+#Installing Cerbot
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+
+sudo apt-get install -y certbot python-certbot-nginx
+sudo certbot --nginx --email $EMAIL
