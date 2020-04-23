cf2dc3c577d9a60acf05bc56749f749a8967f15e
anatoliykv <anatoliykv@gmail.com>
2020-04-23 15:09:52 +0300

    cerbot installing

--- install_nginx_with_LE_SSL.sh
+++ install_nginx_with_LE_SSL.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+set -e
 echo "Enter your domain name"
 read DOMAIN_NAME
-echo "Enter your email"
+echo "Enter your email for Cerbot"
 read EMAIL
 sudo apt update
 sudo apt install nginx -y
 
 #Uncomment server name, 443 port ipv4 and ipv6
-sed -i 's/server_name/server_name $DOMAIN_NAME;/' /etc/nginx/sites-available/default
-sed -i 's/# listen 443 ssl default_server;/listen 443 ssl default_server;/' /etc/nginx/sites-available/default
-sed -i 's/# listen [::]:443 ssl default_server;/listen [::]:443 ssl default_server;/' /etc/nginx/sites-available/default
+sudo sed -i 's/server_name/server_name $DOMAIN_NAME;/' /etc/nginx/sites-available/default
+#sudo sed -i 's/# listen 443 ssl default_server;/listen 443 ssl default_server;/' /etc/nginx/sites-available/default
+#sudo sed -i 's/# listen [::]:443 ssl default_server;/listen [::]:443 ssl default_server;/' /etc/nginx/sites-available/default
 
+nginx -s reload # Checking the correct config and reloading nginx with new config
+if [ $? -ne 0 ]; then
+    echo "Error, Check your Nginx config"
+    exit 1
+else
+    sudo service nginx restart
+fi
 #Installing Cerbot
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository universe
@@ -18,4 +26,12 @@ sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 
 sudo apt-get install -y certbot python-certbot-nginx
-sudo certbot --nginx --email $EMAIL
+#installing cerbot with email, email wount be shared, agree tos and redirect to https
+sudo certbot --nginx -m $EMAIL --no-eff-email -n --agree-tos --domains $DOMAIN_NAME --redirect
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+else
+    sudo service nginx restart
+    echo "Success!"
+fi
