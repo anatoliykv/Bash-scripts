2f08d7ae1720b2f4455dbb89bccd23416e608439
anatoliykv <anatoliykv@gmail.com>
2020-04-25 13:15:40 +0300

    apache added

--- installation_apache_or_nginx_LE_SSL.sh
+++ installation_apache_or_nginx_LE_SSL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Installing NGINX on Debian based distributions 
+# Installing NGINX or Apache2 on Debian based distributions 
 # with Let's Encrypt certificate and renewal it. 
 
 set -e
@@ -24,34 +24,34 @@ sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
 
 if [ $WEB_SERVER = 1 ]; then
-     sudo apt-get install certbot python-certbot-apache
+     sudo apt-get install -y certbot python-certbot-apache
 elif [ $WEB_SERVER = 2 ]; then
      sudo apt-get install -y certbot python-certbot-nginx
 else
      echo "Error"
 fi
 if [ $WEB_SERVER = 1 ]; then
-    server="--apache"
+    server="apache"
 elif [ $WEB_SERVER = 2 ]; then
-    server="--nginx"
+    server="nginx"
 else
     echo "Error"
 fi
 
 #installing cerbot with email, email wount be shared, agree tos and redirect to https
-sudo certbot $server -m $EMAIL --no-eff-email -n --agree-tos --domains $DOMAIN_NAME --redirect
+sudo certbot --"$server" -m "$EMAIL" --no-eff-email -n --agree-tos --domains "$DOMAIN_NAME" --redirect
 if [ $? -ne 0 ]; then
     echo "Error"
     exit 1
 else
-    if [ $WEB_SERVER = 1 ]; then
+    if [ "$WEB_SERVER" = 1 ]; then
     sudo service apache2 restart
     echo "Success!\n
-          Your Apache site is ready!"
-    elif [ $WEB_SERVER = 2 ];then
+          Your Apache web server is ready!"
+    elif [ "$WEB_SERVER" = 2 ];then
     sudo service nginx restart
     echo "Success!\n
-          Your Nginx site is ready!"
+          Your Nginx web server is ready!"
     else
     echo "Error"
     exit 1
@@ -70,35 +70,55 @@ fi
 case "$WEB_SERVER" in
     1)
         echo "You chosed Apache"
+        # Installing Apache
+        sudo apt update
+        sudo apt install apache2 -y
+
+        #Editing Default Apache config
+        sudo sed -i "s/#ServerName www.example.com/ServerName ${DOMAIN_NAME};/" /etc/apache2/sites-available/000-default.conf
+        sudo apache2ctl -t
+        if [ $? -ne 0 ];then
+            echo "Error, check yoyr Apache config"
+            exit 1
+        else
+            sudo systemctl start apache2
+            if [ $? -ne 0 ]; then
+                    echo "Apache can't run sorry!"
+                    exit 1
+                else
+                    echo "Apache started"
+                fi
+        fi
+    certbot
     ;;
     2)
         echo "You chosed Nginx"
         # Installing NGINX
-sudo apt update
-sudo apt install nginx -y
+        sudo apt update
+        sudo apt install nginx -y
 
-#Editing Default Nginx config
-sudo sed -i "s/server_name _;/server_name ${DOMAIN_NAME};/" /etc/nginx/sites-available/default
+        #Editing Default Nginx config
+        sudo sed -i "s/server_name _;/server_name ${DOMAIN_NAME};/" /etc/nginx/sites-available/default
 
-nginx -s reload # Checking the correct config and reloading nginx with new config
-if [ $? -ne 0 ]; then
-    echo "Error, Check your Nginx config"
-    exit 1
-else
-    sudo service nginx restart
-    if [ $? -ne 0 ]; then
-        echo "Nginx can't run sorry!"
-        exit 1
-    else
-        echo "Nginx started"
-    fi
-fi
-cerbot
+        nginx -s reload # Checking the correct config and reloading nginx with new config
+        if [ $? -ne 0 ]; then
+            echo "Error, Check your Nginx config"
+            exit 1
+        else
+            sudo service nginx restart
+                if [ $? -ne 0 ]; then
+                    echo "Nginx can't run sorry!"
+                    exit 1
+                else
+                    echo "Nginx started"
+                fi
+        fi
+    certbot
     ;;
     *)
         echo "You don't chosed web server\n
-        Bye!"
+              Bye!"
         exit 0
     ;;
 esac
-
+sudo apt autoremove -y
