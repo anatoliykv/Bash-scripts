7a53b7779ba94f9fa4a8fd023494241a157c6c0d
anatoliykv <anatoliykv@gmail.com>
2020-04-25 12:14:00 +0300

    added func

---
+++ installation_apache_or_nginx_LE_SSL.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Installing NGINX on Debian based distributions 
+# with Let's Encrypt certificate and renewal it. 
+
+set -e
+echo "Enter your domain name"
+read DOMAIN_NAME
+echo "Enter your email for Certbot"
+read EMAIL
+
+#Chose Apache or Nginx
+echo -e "\e[41mPlease, chose next web server:\e[0m
+\t\t\e[32mApache press 1\e[0m
+\t\t\e[33mNginx  press 2\e[0m"
+
+read WEB_SERVER
+
+function certbot () {
+#Installing Certbot
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository -y ppa:certbot/certbot
+sudo apt-get update
+
+if [ $WEB_SERVER = 1 ]; then
+     sudo apt-get install certbot python-certbot-apache
+elif [ $WEB_SERVER = 2 ]; then
+     sudo apt-get install -y certbot python-certbot-nginx
+else
+     echo "Error"
+fi
+if [ $WEB_SERVER = 1 ]; then
+    server="--apache"
+elif [ $WEB_SERVER = 2 ]; then
+    server="--nginx"
+else
+    echo "Error"
+fi
+
+#installing cerbot with email, email wount be shared, agree tos and redirect to https
+sudo certbot $server -m $EMAIL --no-eff-email -n --agree-tos --domains $DOMAIN_NAME --redirect
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+else
+    if [ $WEB_SERVER = 1 ]; then
+    sudo service apache2 restart
+    echo "Success!\n
+          Your Apache site is ready!"
+    elif [ $WEB_SERVER = 2 ];then
+    sudo service nginx restart
+    echo "Success!\n
+          Your Nginx site is ready!"
+    else
+    echo "Error"
+    exit 1
+    fi
+fi
+#Test renewal cert
+sudo certbot renew --dry-run
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+else
+    echo "Certificate will be renewing"
+fi
+}
+
+case "$WEB_SERVER" in
+    1)
+        echo "You chosed Apache"
+    ;;
+    2)
+        echo "You chosed Nginx"
+        # Installing NGINX
+sudo apt update
+sudo apt install nginx -y
+
+#Editing Default Nginx config
+sudo sed -i "s/server_name _;/server_name ${DOMAIN_NAME};/" /etc/nginx/sites-available/default
+
+nginx -s reload # Checking the correct config and reloading nginx with new config
+if [ $? -ne 0 ]; then
+    echo "Error, Check your Nginx config"
+    exit 1
+else
+    sudo service nginx restart
+    if [ $? -ne 0 ]; then
+        echo "Nginx can't run sorry!"
+        exit 1
+    else
+        echo "Nginx started"
+    fi
+fi
+cerbot
+    ;;
+    *)
+        echo "You don't chosed web server\n
+        Bye!"
+        exit 0
+    ;;
+esac
+
